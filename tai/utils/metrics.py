89ce95ed9077997d51039cc1473822fdbda976d9
Arun S V <arun119019@gmail.com>
2024-01-24 19:29:07 +0530

    Refactor metrics.py: Remove unused imports and add new functions for cross entropy calculations

--- tai/utils/metrics.py
+++ tai/utils/metrics.py
@@ -1,7 +1,5 @@
 import optax
-import tensorflow as tf
 from clu import metrics
-from flax import linen as nn
 from flax import struct
 from flax.training.train_state import TrainState
 from jax import numpy as jnp
@@ -17,20 +15,20 @@ class TrainState(TrainState):
     metrics: Metrics
 
 
-# def loss_fn(rngs, state, inputs, targets, labels):
-#     preds = state.apply_fn({"params": state.params}, inputs, targets, rngs=rngs)
-#     preds = nn.log_softmax(preds)
-#     mask = jnp.not_equal(jnp.sum(labels, axis=-1), 0)
-#     preds = preds[mask]
-#     labels = labels[mask]
-#     return -jnp.sum(preds * labels, axis=-1)
+def cross_entropy_with_label_smoothing(preds, labels, smoothing=0.1):
+    """
+    NOTE: This function is not jit-able.
+    """
+    confidence = 1.0 - smoothing
+    non_zero_count = jnp.count_nonzero(labels)
+    preds, labels = preds[:non_zero_count], labels[:non_zero_count]
+    true_dist = jnp.full_like(preds, smoothing / (preds.shape[-1] - 2))
+    true_dist = true_dist.at[:, labels].set(confidence)
+    true_dist.at[:, :2].set(0.0)
+    return optax.softmax_cross_entropy(preds, true_dist).mean()
 
 
-def smooth_labels(labels, num_labels, smoothing=0.1):
-    labels = tf.expand_dims(labels, axis=-1)
-    labels = tf.one_hot(labels, depth=num_labels)
-    return tf.where(
-        labels[..., 0] == 1,
-        tf.zeros(num_labels),
-        (1 - smoothing) * labels / (num_labels - 1),
-    )
+def cross_entropy_with_integer_labels(preds, labels):
+    non_zero_count = jnp.count_nonzero(labels)
+    preds, labels = preds[:non_zero_count], labels[:non_zero_count]
+    return optax.softmax_cross_entropy_with_integer_labels(preds, labels).mean()
