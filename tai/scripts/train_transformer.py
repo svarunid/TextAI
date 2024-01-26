Fix loss and grad calculation

tai/scripts/train_transformer.py
@@ -5,7 +5,6 @@ import jax
 import optax
 import wandb
 import yaml
-from clu.data import TfDatasetIterator
 from etils import epath
 from jax import config as jax_config
 from jax import numpy as jnp
@@ -13,11 +12,12 @@ from orbax import checkpoint as ocp
 
 from tai.models.transformer import Transformer, TransformerConfig
 from tai.utils import metrics
-from tai.utils.data import create_dataset
+from tai.utils.data import TfDatasetIterator, create_dataset
 
 # Configuring JAX & tensorflow
 jax_config.update("jax_debug_nans", True)
 jax_config.update("jax_debug_infs", True)
+
 
 # Loading configuration
 root_dir = epath.Path(os.path.dirname(os.path.realpath(__file__))).parent.parent
@@ -25,6 +25,7 @@ config_dir = root_dir / "config" / "transformer.yaml"
 with open(config_dir, "r") as f:
     config = yaml.load(f, Loader=yaml.Loader)
     del config_dir
+
 
 seed = config["seed"]
 model_config = config["model"]
@@ -35,28 +36,33 @@ checkpoint_config = config["checkpoint"]
 optimizer_config = config["optimizer"]
 del config
 
+
 # Initialize dataset
 tok_config["path"] = root_dir / tok_config["path"]
 train_config["cache_dir"] = root_dir / train_config["cache_dir"]
+if not train_config["cache_dir"].exists():
+    train_config["cache_dir"].mkdir(parents=True, exist_ok=True)
 src = root_dir / train_config["path"] / train_config["src"]
 tgt = root_dir / train_config["path"] / train_config["tgt"]
 dataloader = TfDatasetIterator(
     create_dataset(train_config, tok_config, src, tgt),
-    checkpoint=checkpoint_config["use_checkpoint"],
+    checkpoint_dir=checkpoint_config["path"],
 )
 del src, tgt
 
+
 # Initialize transformer model
-param_key, dropout_key = jax.random.split(jax.random.PRNGKey(config["seed"]))
+param_key, dropout_key = jax.random.split(jax.random.PRNGKey(seed))
 model = Transformer(TransformerConfig.fromDict(model_config))
 params = model.init(
     {
         "params": param_key,
         "dropout": dropout_key,
     },
-    jnp.ones((train_config["src_max_len"]), dtype=jnp.int32),
-    jnp.ones((train_config["tgt_max_len"]), dtype=jnp.int32),
+    jnp.ones((train_config["src_max_len"],), dtype=jnp.int32),
+    jnp.ones((train_config["tgt_max_len"],), dtype=jnp.int32),
 )
+
 
 # Defining optimiser
 lr = optax.warmup_cosine_decay_schedule(
@@ -66,8 +72,9 @@ lr = optax.warmup_cosine_decay_schedule(
     optimizer_config["decay_steps"],
     optimizer_config["final_lr"],
 )
-optimizer = optax.inject_hyperparams(optax.adam)(learning_rate=lr)
+optimizer = optax.adam(lr)
 del lr
+
 
 # Initialize training state
 state = metrics.TrainState.create(
@@ -82,27 +89,25 @@ del params, optimizer
 # Define a train step function
 @jax.jit
 def train_step(state, inputs, targets, labels):
-    loss_and_grad = jax.value_and_grad(
-        jax.vmap(metrics.cross_entropy_with_integer_labels)
-    )
-    preds = state.apply_fn({"params": state.params}, inputs, targets)
-    loss, grads = loss_and_grad(preds=preds, labels=labels)
-    loss = jnp.mean(loss)
-    grads = jax.tree_util.tree_map(lambda x: jnp.mean(x, axis=0), grads)
+    def loss_fn(params):
+        preds = state.apply_fn(params, inputs, targets, rngs={"dropout": dropout_key})
+        return jnp.mean(
+            jax.vmap(metrics.cross_entropy_with_integer_labels)(preds, labels)
+        )
+
+    grad_fn = jax.value_and_grad(loss_fn)
+    loss, grads = grad_fn(state.params)
     return state.apply_gradients(grads=grads), loss
 
 
 @jax.jit
 def compute_metrics(state, inputs, targets, labels):
-    loss_fn = jax.vmap(metrics.cross_entropy_with_integer_labels)
-    preds = state.apply_fn({"params": state.params}, inputs, targets)
-    loss = loss_fn(preds=preds, labels=labels)
-    non_zero_count = jnp.count_nonzero(labels)
-    preds, labels = nn.softmax(preds[:non_zero_count]), labels[:non_zero_count]
+    preds = state.apply_fn(state.params, inputs, targets, rngs={"dropout": dropout_key})
+    loss = jnp.mean(jax.vmap(metrics.cross_entropy_with_integer_labels)(preds, labels))
+    accuracy = jnp.mean(jax.vmap(metrics.accuracy)(preds, labels))
     metrics_updates = state.metrics.single_from_model_output(
-        logits=preds,
-        labels=labels,
         loss=loss,
+        accuracy=accuracy,
     )
     metrics = state.metrics.merge(metrics_updates)
     return state.replace(metrics=metrics)
@@ -111,30 +116,26 @@ def compute_metrics(state, inputs, targets, labels):
 # Make checkpoint directory if it doesn't exist and initialize checkpoint manager
 if use_checkpoint := checkpoint_config["use_checkpoint"]:
     freq = checkpoint_config["freq"]
-    state_checkpoint_path = root_dir / checkpoint_config["path"]
-    dataset_checkpoint_path = state_checkpoint_path / "dataset"
-    if state_checkpoint_path.exists() and checkpoint_config["overwrite"]:
-        state_checkpoint_path.rmtree()
-        state_checkpoint_path.mkdir(parents=True, exist_ok=True)
+    checkpoint_path = root_dir / checkpoint_config["path"]
+    if checkpoint_path.exists() and checkpoint_config["overwrite"]:
+        checkpoint_path.rmtree()
+        checkpoint_path.mkdir(parents=True, exist_ok=True)
     options = ocp.CheckpointManagerOptions(
         max_to_keep=checkpoint_config["max_to_keep"],
         save_interval_steps=freq,
     )
     mngr = ocp.CheckpointManager(
-        state_checkpoint_path.resolve(),
-        {"state": state},
-        options,
+        checkpoint_path.resolve(),
+        options=options,
     )
-    del state_checkpoint_path, options
+    del checkpoint_path, options
 
-    # Loading model, optimiser state and training loop config if checkpoint exists
+    # Loading state and dataloader if checkpoint exists
     if mngr.latest_step() is not None:
         mngr.wait_until_finished()
-        restored_items = mngr.restore(mngr.latest_step())
-        state = restored_items["state"]
+        state = mngr.restore(mngr.latest_step())
+        dataloader = dataloader.restore("dataset")
 
-    if dataset_checkpoint_path.exists():
-        dataloader = dataloader.restore(dataset_checkpoint_path)
 
 # Optional configuration for logging to wandb
 if use_wandb := wandb_config["use_wandb"]:
@@ -153,6 +154,7 @@ if use_wandb := wandb_config["use_wandb"]:
     )
     del wandb_config
 
+
 # Training loop
 print("Running...")
 for i, (inputs, targets, labels) in enumerate(dataloader):
@@ -164,9 +166,9 @@ for i, (inputs, targets, labels) in enumerate(dataloader):
     metrics = state.metrics.compute()
 
     # Checkpoint model and optimiser state
-    if use_checkpoint:
-        mngr.save(i, items={"state": state})
-        dataloader.save(dataset_checkpoint_path)
+    if use_checkpoint and i % freq == 0:
+        mngr.save(i, args=ocp.args.PyTreeSave(item=state))
+        dataloader.save("dataset")
 
     # Log to wandb
     if use_wandb:

tai/utils/data.py
@@ -1,6 +1,10 @@
+import os
 from functools import partial
 
+import jax
 import tensorflow as tf
+from clu import data
+from etils.epath import Path
 from tensorflow_text import SentencepieceTokenizer
 
 AUTOTUNE = tf.data.AUTOTUNE
@@ -22,6 +26,7 @@ def create_tokenizer(config):
 
 def create_dataset(config, tok_config, src, tgt):
     map_with_autotune = lambda ds: partial(ds.map, num_parallel_calls=AUTOTUNE)
+    to_dlpack = tf.experimental.dlpack.to_dlpack
     src_ds = tf.data.TextLineDataset(src)
     tgt_ds = tf.data.TextLineDataset(tgt)
     src_tok, tgt_tok = create_tokenizer(tok_config)
@@ -29,7 +34,7 @@ def create_dataset(config, tok_config, src, tgt):
     tgt_ds = map_with_autotune(tgt_ds)(tgt_tok.tokenize)
     src_ds = map_with_autotune(src_ds)(lambda x: x[: config["src_max_len"]])
     tgt_ds = map_with_autotune(tgt_ds)(
-        lambda x: tf.concat([[2], x[: config["tgt_max_len"]]], axis=-1)
+        lambda x: tf.concat([[2], x[: config["tgt_max_len"] - 1]], axis=-1)
     )
     labels_ds = map_with_autotune(tgt_ds)(
         lambda x: tf.concat([x[1:], [3]], axis=-1),
@@ -37,7 +42,6 @@ def create_dataset(config, tok_config, src, tgt):
 
     ds = (
         tf.data.Dataset.zip((src_ds, tgt_ds, labels_ds))
-        .cache(filename=str(config["cache_dir"]))
         .padded_batch(
             config["batch_size"],
             padded_shapes=(
@@ -46,6 +50,41 @@ def create_dataset(config, tok_config, src, tgt):
                 config["tgt_max_len"],
             ),
         )
+        .cache(filename=str(config["cache_dir"]))
         .repeat(config["epochs"])
     )
+
     return ds
+
+
+class TfDatasetIterator(data.DatasetIterator):
+    def __init__(self, dataset, checkpoint_dir) -> None:
+        super().__init__()
+        self._dataset = dataset
+        self.checkpoint_dir = Path(checkpoint_dir)
+        self.iterator = iter(dataset)
+        self._checkpointer = tf.train.Checkpoint(ds=self.iterator)
+
+    @property
+    def element_spec(self):
+        elememt_spec = self._dataset.element_spec
+
+        return tuple(
+            data.ArraySpec(dtype=el.dtype.as_numpy_dtype, shape=tuple(el.shape))
+            for el in elememt_spec
+        )
+
+    def __next__(self):
+        to_dlpack = tf.experimental.dlpack.to_dlpack
+        to_array = jax.dlpack.from_dlpack
+        return tuple(to_array(to_dlpack(el)) for el in next(self.iterator))
+
+    def reset(self):
+        self.iterator = iter(self._dataset)
+        self._checkpointer = tf.train.Checkpoint(ds=self.iterator)
+
+    def save(self, filename: Path):
+        self._checkpointer.write(file_prefix=os.fspath(self.checkpoint_dir / filename))
+
+    def restore(self, filename: Path):
+        self._checkpointer.read(os.fspath(self.checkpoint_dir / filename))

tai/utils/metrics.py
@@ -1,4 +1,4 @@
-import jax
+import flax.linen as nn
 import optax
 from clu import metrics
 from flax import struct
@@ -9,7 +9,7 @@ from jax import numpy as jnp
 @struct.dataclass
 class Metrics(metrics.Collection):
     loss: metrics.Average.from_output("loss")
-    accuracy: metrics.Accuracy
+    accuracy: metrics.Average.from_output("accuracy")
 
 
 class TrainState(TrainState):
@@ -30,6 +30,14 @@ def cross_entropy_with_label_smoothing(preds, labels, smoothing=0.1):
 
 
 def cross_entropy_with_integer_labels(preds, labels):
-    non_zero_count = jnp.count_nonzero(labels)
-    preds, labels = preds[:non_zero_count], labels[:non_zero_count]
-    return optax.softmax_cross_entropy_with_integer_labels(preds, labels).mean()
+    preds = nn.log_softmax(preds)
+    preds = jnp.where(labels == 0, 0, jnp.take(preds, labels, axis=-1))
+    count = jnp.count_nonzero(preds)
+    return -jnp.sum(preds) / count
+
+
+def accuracy(preds, labels):
+    preds = nn.softmax(preds)
+    preds = jnp.where(labels == 0, 0, jnp.argmax(preds, axis=-1))
+    count = jnp.count_nonzero(labels)
+    return jnp.sum(preds == labels) / count