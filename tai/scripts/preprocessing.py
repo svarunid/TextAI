8bc77c3063433d794cc8357d2ac89f2ff473a53f
Arun S V <arun119019@gmail.com>
2024-02-05 13:56:38 +0530

    Updated MLM preprocessing

--- tai/scripts/preprocessing.py
+++ tai/scripts/preprocessing.py
@@ -1,30 +1,30 @@
+import os
 from functools import partial
-from os import makedirs, path
 from pathlib import Path
 
 import sentencepiece as spm
 import yaml
 
-root_dir = Path(path.dirname(path.realpath(__file__))).parent.parent
+root_dir = Path(os.path.dirname(os.path.realpath(__file__))).parent.parent
 config_dir = root_dir / "config" / "preprocessing.yaml"
 with open(config_dir, "r") as f:
     config = yaml.load(f, Loader=yaml.Loader)
 
-input_type = config["input_type"]
 data_path = Path(config["path"])
 
-if input_type == "text":
-    data_config = config[input_type]
+task = config["task"]
+task_config = config[task]
 
-    task = data_config["task"]
-    task_config = data_config[task]
-
-    train_tokenizer = partial(
-        spm.SentencePieceTrainer.train,
-        model_type=data_config["model"],
-        normalization_rule_name=data_config["norm_rule"],
-        shuffle_input_sentence=data_config["shuffle"],
-        input_sentence_size=data_config["sample_size"],
+def train_tokenizer(dir, vocab_size, control_symbols=[]):
+    spm.SentencePieceTrainer.train(
+        input=os.fspath(root_dir / data_path / dir),
+        model_prefix=os.fspath(root_dir / data_path / dir.split(".")[0]),
+        vocab_size=vocab_size,
+        control_symbols=control_symbols,
+        model_type=config["model"],
+        normalization_rule_name=config["norm_rule"],
+        shuffle_input_sentence=config["shuffle"],
+        input_sentence_size=config["sample_size"],
         pad_id=0,
         unk_id=1,
         bos_id=2,
@@ -32,19 +32,19 @@ if input_type == "text":
         unk_surface="<unk>",
     )
 
-    tok_path = Path("tokenizer").joinpath(*data_path.parts[1:])
 
-    if not tok_path.exists():
-        makedirs(tok_path, exist_ok=True)
+tok_path = Path("tokenizer").joinpath(*data_path.parts[1:])
 
-    if task == "seq2seq":
+if not tok_path.exists():
+    tok_path.mkdir(parents=True, exist_ok=True)
+
+match task:
+    case "seq2seq":
+        train_tokenizer(task_config["src"], task_config["src_vocab"])
+        train_tokenizer(task_config["tgt"], task_config["tgt_vocab"])
+    case "mlm":
         train_tokenizer(
-            input=str((root_dir / data_path / task_config["src"])),
-            model_prefix=str((root_dir / tok_path / task_config["src"].split(".")[0])),
-            vocab_size=task_config["src_vocab"],
-        )
-        train_tokenizer(
-            input=str((root_dir / data_path / task_config["tgt"])),
-            model_prefix=str((root_dir / tok_path / task_config["tgt"].split(".")[0])),
-            vocab_size=task_config["tgt_vocab"],
+            task_config["src"], 
+            task_config["vocab"],
+            control_symbols=[task_config["cls_token"], task_config["sep_token"]]
         )
